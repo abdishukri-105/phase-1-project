97351393c728901584ca40b4fde3610fd1c07a20
abdishukri <abdishukrimohamed105@gmail.com>
2022-12-17 20:43:57 +0300

    added technews section

--- src/index.js
+++ src/index.js
@@ -1,240 +1,82 @@
-// // // console.log("shukri")
+//apis
+const SPORTS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=";
+const ENTERTAINMENT_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=";
+const TECHNOLOGY_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=technology&pageSize=8&apiKey=";
+// api key
+const API_KEY = '1c5ef8925c7549d5aa78b4ddb9563a20';
 
 
 
-// // // // 1c5ef8925c7549d5aa78b4ddb9563a20 --- api key
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '9b7e8da5e2msh065d9972ad16fcdp175dfdjsn726f8012b370',
-        'X-RapidAPI-Host': 'news-api14.p.rapidapi.com'
-    }
-};
+const technologyBtn = document.getElementById("technology");
 
-// document.addEventListener('DOMContentLoaded', () =>{
 
+
+// display technology news on click
+technologyBtn.addEventListener("click",function(){
+    newsType.innerHTML="<h4>Technology</h4>";
+    fetchTechnologyNews();
+});
+
+
+
+// fetch tech news
+const fetchTechnologyNews = async () => {
    
-
-//         // fetch('https://news-api14.p.rapidapi.com/top-headlines?country=england&language=en&pageSize=10&category=business&sortBy=timestamp', options)
-//         // .then(response => response.json())
-//         // .then(data => {
-//         //     const card = document.getElementById('imgUrl')
-//         //     console.log(card)
-//         //     console.log(data)
-//         //     card.src = data.url 
-//         //     const cardtitle = document.getElementById('cardtitle')
-//         //     console.log(cardtitle)
-//         //     cardtitle.textContent = data.title
-//         //     console.log(cardtitle.title)
-//         //     const cardInfo = document.querySelector('#cardText')
-//         //     cardInfo.textContent = data.url
-//         //     body.appendchild(card)
-//         // })
-//         // .catch(err => console.error(err));
-
-
-//         const card = (image, name) => {
-
-//             const rootDiv = document.createElement('div')
-//             rootDiv.classList.add('col-4', 'p-1')
+	fetch(TECHNOLOGY_NEWS+API_KEY)
+	.then(response => response.json())
+	.then(news =>{
+		console.log(news)
+		displayNews(news)
+	})
     
-//             const cardDiv = document.createElement('div')
-//             cardDiv.classList.add('card', 'col-12', 'p-2')
-    
-//             const categoryImg = document.createElement('img')
-//             categoryImg.classList.add('card-img-top')
-//             categoryImg.src = image
-    
-//             const categoryTitle = document.createElement('h4')
-//             categoryTitle.classList.add('card-title')
-//             categoryTitle.innerText = name
-    
-//             // append title and image to card
-//             cardDiv.appendChild(categoryImg)
-//             cardDiv.appendChild(categoryTitle)
-    
-//             rootDiv.appendChild(cardDiv)
-    
-//             return rootDiv
-    
-//         }
-  
-//         const loadcard = () => {
-//             fetch('https://news-api14.p.rapidapi.com/top-headlines?country=england&language=en&pageSize=10&category=business&sortBy=timestamp', options)
-//                 .then((response) => response.json())
-//                 .then((countries) => {
-//                     const mealCountries = countries.meals
-    
-//                     const mealElems = mealCountries.map(
-//                         area => c (area.strArea)
-//                     )
-                    
-//                     countriesRow.append(...mealElems)
-//                 })
-//         }
-
-
-// loadcard()
-// card()
-
-// })
+}
 
 
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '9b7e8da5e2msh065d9972ad16fcdp175dfdjsn726f8012b370',
-// 		'X-RapidAPI-Host': 'news-api14.p.rapidapi.com'
-// 	}
-// };
+// render news 
+function displayNews(news) {
 
-// fetch('https://news-api14.p.rapidapi.com/top-headlines?country=us&language=en&pageSize=10&category=sports', options)
-// 	.then(response => response.json())
-// 	.then(data =>{
-//         console.log(data)
-//     } )
-// 	.catch(err => console.error(err));
+    newsdetails.innerHTML = "";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // const options = {
-// // 	method: 'GET',
-// // 	headers: {
-// // 		'X-RapidAPI-Key': '9b7e8da5e2msh065d9972ad16fcdp175dfdjsn726f8012b370',
-// // 		'X-RapidAPI-Host': 'news-api14.p.rapidapi.com'
-// // 	}
-// // };
-
-// // fetch('https://news-api14.p.rapidapi.com/top-headlines?country=england&language=en&pageSize=10&category=sports&sortBy=timestamp', options)
-// // 	.then(response => response.json())
-// // 	.then(data => 
-// //         {
-// //             data.
-
-// //         })
-// // 	.catch(err => console.error(err));
-
-
-
-   
-    
-   
-
-      
+    news.articles.forEach(article => {
         
-// //         fetch('https://news-api14.p.rapidapi.com/top-headlines?language=en&pageSize=10&category=technology&sortBy=timestamp', options)
-// //             .then(response => response.json())
-// //             .then(response => console.log(response))
-// //             .catch(err => console.error(err));
+        const col = document.createElement('div');
+        col.className="col-sm-12 col-md-4 col-lg-3 p-2 card";
 
-          
-    
-            
-// //             fetch('https://news-api14.p.rapidapi.com/search?q=computer&language=en&pageSize=10&publisher=cnn.com%2Cbbc.com&sortBy=timestamp', options)
-// //                 .then(response => response.json())
-// //                 .then(response => console.log(response))
-// //                 .catch(err => console.error(err));
+        const card = document.createElement('div');
+        card.className = "p-2";
 
+        const image = document.createElement('img');
+        image.setAttribute("height","matchparent");
+        image.setAttribute("width","100%");
+        image.src=article.urlToImage;
 
+        const cardBody = document.createElement('div');
+        
+        const newsHeading = document.createElement('h5');
+        newsHeading.className = "card-title";
+        newsHeading.innerHTML = article.title;
 
-// // 
-//   const ul = document.getElementById('authors');
-//   const list = document.createDocumentFragment();
-//   const url = 'https://jsonplaceholder.typicode.com/users';
-//   const URL5 = 'https://news-api14.p.rapidapi.com/top-headlines?language=en&pageSize=10&category=technology&sortBy=timestamp'
+        const discription = document.createElement('p');
+        discription.className="text-muted";
+        discription.innerHTML = article.description;
 
-//   fetch(URL5,options)
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       let authors = data;
-// 	  console.log(authors)
+        const link = document.createElement('a');
+        link.className="btn btn-dark";
+        link.setAttribute("target", "_blank");
+        link.href = article.url;
+        link.innerHTML="Read more";
 
+        cardBody.appendChild(newsHeading);
+        cardBody.appendChild(discription);
+        cardBody.appendChild(link);
 
-//       authors.map(function(author) {
-//         let li = document.createElement('li');
-//         let name = document.createElement('h2');
-//         let email = document.createElement('span');
+        card.appendChild(image);
+        card.appendChild(cardBody);
 
-//         name.innerHTML = `${author.name}`;
-//         email.innerHTML = `${author.email}`;
+        col.appendChild(card);
 
-//         li.appendChild(name);
-//         li.appendChild(email);
-//         list.appendChild(li);
-//       });
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     });
+        newsdetails.appendChild(col);
+    });
 
-//   ul.appendChild(list);
-
-
-const searchForm = document.querySelector('#search-form')
-const input = document.querySelector('#search')
-const newsList = document.querySelector('.class-list')
-// console.log(searchForm)
-
-searchForm.addEventListener('submit',retrieve)
-
-function retrieve(e) {
-    // load new data on new search
-	// newsList.innerHTML = ''
-
-	e.preventDefault()
-
-	// alert when input field is empty
-	if (input.value == ''){
-		alert('please search')
-		return
-	}
-
-	const apiKey = '1c5ef8925c7549d5aa78b4ddb9563a20'
-    let topic = input.value 
-	const url =`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
-   
-	fetch(url).then(res => res.json())
-	.then(data => {
-		console.log(data)
-	 data.articles.forEach(article =>{
-
-		const imageholder = document.getElementById('urlToImage')
-		const title = document.getElementById('title')
-		const description = document.getElementById('description')
-		const a = document.getElementById('readmore')
-		a.setAttribute('href', article.url)
-		a.setAttribute('target', '-blank')
-
-		a.textContent = article.url
-		imageholder.src = article.urlToImage
-		title.textContent = article.title
-		description.textContent = article.description
-
-		
-
-	 })
-	}).catch(error => console.log(error))
-	
-	console.log(topic)
 }
